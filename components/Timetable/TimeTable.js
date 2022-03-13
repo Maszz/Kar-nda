d6e35842c39557c7454257a070eaf9d94e67e5cd
Masz <mawin38408@gmail.com>
2022-03-13 15:03:23 +0700

    upgrade module

--- components/Timetable/TimeTable.js
+++ components/Timetable/TimeTable.js
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
-import { View, Text, Dimensions } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { Calendar } from 'react-native-big-calendar';
+import React, {useState} from 'react';
+import {View, Text, Dimensions} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {Calendar} from 'react-native-big-calendar';
 import {
   createDrawerNavigator,
   DrawerContentScrollView,
@@ -12,7 +12,7 @@ import dayjs from 'dayjs';
 import Icon from 'react-native-vector-icons/dist/Ionicons';
 import CalendarStrip from 'react-native-calendar-strip';
 import moment from 'moment';
-import { ZStack, Box } from 'native-base';
+import {ZStack, Box} from 'native-base';
 import TimeTableBody from './TimeTableBody';
 
 const TimeTable = props => {
@@ -22,9 +22,13 @@ const TimeTable = props => {
       <CalendarStrip
         scrollable
         selectedDate={new Date(Date.now())}
-        style={{ height: 90, paddingTop: 10, marginBottom: 10 }}
-        calendarAnimation={{ type: 'sequence', duration: 30 }}
-        style={{ height: 65, paddingHorizontal: 5 }}
+        style={{
+          height: 90,
+          paddingTop: 10,
+          marginBottom: 10,
+          paddingHorizontal: 5,
+        }}
+        calendarAnimation={{type: 'sequence', duration: 30}}
         daySelectionAnimation={{
           type: 'border',
           duration: 200,
@@ -36,12 +40,12 @@ const TimeTable = props => {
           console.log(e);
           setCurrentDate(e);
         }}
-        highlightDateNameStyle={{ color: 'red' }}
-        highlightDateNumberStyle={{ color: 'red' }}
-        dateNumberStyle={{ color: 'white' }}
-        dateNameStyle={{ color: 'white' }}
-        calendarHeaderStyle={{ color: 'white', display: 'none' }}
-        iconContainer={{ flex: 0.1 }}
+        highlightDateNameStyle={{color: 'red'}}
+        highlightDateNumberStyle={{color: 'red'}}
+        dateNumberStyle={{color: 'white'}}
+        dateNameStyle={{color: 'white'}}
+        calendarHeaderStyle={{color: 'white', display: 'none'}}
+        iconContainer={{flex: 0.1}}
       />
       <TimeTableBody currentDate={currentDate} events={props.events} />
     </View>
