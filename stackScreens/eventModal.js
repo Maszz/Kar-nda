9d6de168410bb8887902585fa28873dd2876fd59
Masz <mawin38408@gmail.com>
2022-03-29 22:18:35 +0700

    fix calendar components

---
+++ stackScreens/eventModal.js
@@ -0,0 +1,45 @@
+import React, {useEffect, useRef, useState} from 'react';
+import {
+  actions,
+  getContentCSS,
+  RichEditor,
+  RichToolbar,
+} from 'react-native-pell-rich-editor';
+import {
+  Button,
+  Box,
+  FormControl,
+  Stack,
+  View,
+  Text,
+  Input,
+  WarningOutlineIcon,
+  TextArea,
+  HStack,
+  Container,
+  VStack,
+  Spacer,
+  Divider,
+} from 'native-base';
+import {
+  Alert,
+  TouchableWithoutFeedback,
+  Keyboard,
+  KeyboardAvoidingView,
+} from 'react-native';
+import {ScrollView} from 'react-native-gesture-handler';
+import moment from 'moment';
+import {useDispatch, useSelector, connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
+import {actionCreators} from '../state/index';
+import * as RNLocalize from 'react-native-localize';
+
+const eventModal = () => {
+  return (
+    <View>
+      <Box>world</Box>
+    </View>
+  );
+};
+
+export default eventModal;
